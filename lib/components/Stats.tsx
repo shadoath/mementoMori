9881fc3ce6a41d5b9dd57570ed10e5381e1b8394
Skylar Bolton <skylar.bolton@gmail.com>
2024-09-16 08:01:51 -0400

    Correct stats logic for weeks lived of % expected

--- lib/components/Stats.tsx
+++ lib/components/Stats.tsx
@@ -1,14 +1,25 @@
 import { useBaseContext } from '../../context/BaseContext'
 import { getWeeksLeft } from '../../app/functions'
+import { useMemo } from 'react'
 
 export const Stats = () => {
-  const { birthdate, lifeExpectancy, totalWeeksInLife } = useBaseContext()
-  const weeksLeft = getWeeksLeft(birthdate, totalWeeksInLife)
-  const percentOfLifeLived = Math.round(
-    ((Math.ceil(totalWeeksInLife) - Math.ceil(weeksLeft)) /
-      Math.ceil(totalWeeksInLife)) *
-      100
-  )
+  const { birthdate, lifeExpectancy } = useBaseContext()
+  const totalWeeksInLife = lifeExpectancy * 52 // Calculate total weeks in life based on life expectancy
+
+  const { percentOfLifeLived, weeksLeft } = useMemo(() => {
+    console.log('stats', { birthdate, totalWeeksInLife })
+    const weeksLeft = getWeeksLeft(birthdate, totalWeeksInLife)
+    const weeksLived = totalWeeksInLife - weeksLeft
+    const percentOfLifeLived = Math.round((weeksLived / totalWeeksInLife) * 100)
+    console.log('stats', {
+      birthdate,
+      totalWeeksInLife,
+      percentOfLifeLived,
+      weeksLeft,
+    })
+    return { percentOfLifeLived, weeksLeft }
+  }, [birthdate, totalWeeksInLife])
+
   return (
     <div id='stats' className='stats wrapper'>
       {weeksLeft !== null &&
